f298540cb8d18ad636059e2c1b83d7b25f8e17a9
MarkVN2 <markosvinicius.nunes@gmail.com>
2023-05-03 13:28:40 -0300

    Setting up the repository

---
+++ src/player/scripts/PlayerMovement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement: MonoBehaviour
+{
+    [SerializeField] public float MoveSpeed = 0f; // Movement Speed
+    [SerializeField] public float DashSpeed = 0f;// Dash Speed
+}
