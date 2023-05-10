d128e7e24beb926bb7e6a035e788898cb66ce161
MarkVN2 <markosvinicius.nunes@gmail.com>
2023-05-10 18:05:25 -0300

    Fixed character movement
    Coding mistakes made the code not work as intended now it works fine.
    
    Character can jump, walk and "slide".
    
    The Slide is not complete therefore the slide just gives a speed boost while in the ground.

--- src/player/PlayerMovement.cs
+++ src/player/PlayerMovement.cs
@@ -1,69 +1,78 @@
+
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+
+    //TODO
+    /* 
+        Create Speed multiplier while in the air;
+        Create Dash;
+        Make collider and camera go down if Sliding;
+    */
 
     public Animator animator;                   // sets the animator as Animator, use this for animations in the code.
 
     [Header("Movement Settings")]
     
-    public float moveSpeed = 60f;               // float for speed while moving 
+    public float moveSpeed = 45f;               // float for speed while moving 
     public float slideSpeed = 1.5f;             // float for speed multiplier while sliding
-    public float jumpForce = 8.0f;              // 
+    public float jumpForce = 13f;               // 
     public float gravity = 20f;                 //
+    public float maxSpeedMultiplier = 2f;       // NOT IN USE FOR NOW
 
-    bool isJumping = false;                          
-    bool isDashing = false;                          
-    bool isSliding = false;                         
-    
+    float multiplier = 1f;
+    bool isJumping;                          
+    bool isDashing;                          
+    bool isSliding;                         
+
     [Header("Camera Settings")]
 
     public Camera playerCamera;                 // sets the player camera as Camera
-
     public float lookSpeed = 2.0f;              // 
-    public float lookXLimit = 45.0f;            // Limits X axis of the camera to a certain angle
-
+    public float lookXLimit = 90.0f;            // Limits axis of the camera to a certain angle
     CharacterController charController;         //
-
     Vector3 moveDirection = Vector3.zero;       //
     float rotationX = 0;                        //
 
     [HideInInspector]
     public bool canMove = true;                 //
     bool inAir;                                 // 
-    private GameObject capsule;
 
     private void Start()
     {
         charController = GetComponent<CharacterController>();       
         
         Cursor.lockState = CursorLockMode.Locked;                 
-        Cursor.visible = false;                                    
+        Cursor.visible = false;                          
+
     }
     private void Update()
     {
-        bool isCrouching = Input.GetKey(KeyCode.LeftControl);
-        bool isSliding = input.GetKey(KeyCode.LeftControl);
-
-        Vector3 forward = transform.transformDirection(Vector3.forward);
-        Vector3 right  = transform.transformDirection(Vector3.right);
         
-        float curSpeedX = canMove ? (isSliding ? moveSpeed * slideSpeed : moveSpeed) * input.GetAxis("Vertical"): 0;
-        float curSpeedY = canMove ? (isSliding ? moveSpeed * slideSpeed : moveSpeed) * input.GetAxis("Horizontal"): 0;
+        bool inAir = (charController.isGrounded) ? false : true;
+
+        bool isSliding = Input.GetKey(KeyCode.LeftControl);
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right  = transform.TransformDirection(Vector3.right);
+        
+        float curSpeedX = canMove ?  moveSpeed  * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ?  moveSpeed  * Input.GetAxis("Horizontal") : 0;
 
         float movementDirectionY = moveDirection.y;
 
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-
-        if (charController.isGrounded){            
-            if(Input.GetButton("Jump")){
-                moveDirection.y  = jumpForce;
-            }
-        }
+        moveDirection = (forward * (curSpeedX + (isSliding ? slideSpeed : 0)) ) + (right * curSpeedY ) ;
 
         movementDirectionY  -= gravity  * Time.deltaTime;
         moveDirection.y = movementDirectionY;
         
+        charController.Move(moveDirection * Time.deltaTime * multiplier);
+        
+        Debug.Log("Grounded:" + (charController.isGrounded));
+        Debug.Log("In Air:" + (inAir));
+
         //Camera rotation   
         if (canMove)
         {
@@ -76,4 +85,14 @@ public class PlayerMovement : MonoBehaviour
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);     //
         }
     }
+     private void FixedUpdate()
+        {
+        if (charController.isGrounded){ 
+            if(Input.GetButton("Jump")){
+                moveDirection.y  = jumpForce;
+            }
+          }
+        }
+
 }
+
